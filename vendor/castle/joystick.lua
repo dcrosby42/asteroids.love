2778dacb6ee0f924d5b00b5d73fb0f6341dae5e2
David Crosby <dcrosby42@gmail.com>
2024-06-09 22:12:57 -0400

    Add proper PS4 Controller joystick control map

--- vendor/castle/joystick.lua
+++ vendor/castle/joystick.lua
@@ -1,5 +1,12 @@
 local Joystick = {}
 
+-- PS4 R2 and L2 triggers report as axis type controls.
+-- They report from -1 to 1, with 0 somewhere around half-pulled.
+-- This func adjusts the range from: rest position at 0, full-pull at 1.
+local function normalizePs4TriggerAccess(value)
+  return (value + 1) / 2
+end
+
 local Defs = {
   {
     name = "PS4 Controller",
@@ -8,77 +15,31 @@ local Defs = {
       { 2, "lefty" },
       { 3, "rightx" },
       { 4, "righty" },
-      { 5, "l2" },
-      { 6, "r2" },
+      { 5, "l2",    { transform = normalizePs4TriggerAccess } },
+      { 6, "r2",    { transform = normalizePs4TriggerAccess } },
     },
     buttons = {
-      { 1,  "face1",   "cross" },
-      { 2,  "face2",   "circle" },
-      { 3,  "face3",   "square" },
-      { 4,  "face4",   "triangle" },
-      { 5,  "select",  "share" },
-      { 6,  "power",   "ps" },
-      { 7,  "start",   "pause" },
+      { 1,  "face1", },
+      { 2,  "face2", },
+      { 3,  "face3", },
+      { 4,  "face4", },
+      { 5,  "select", },
+      { 6,  "power", },
+      { 7,  "start", },
       { 8,  "l3" },
       { 9,  "r3" },
       { 10, "l1" },
       { 11, "r1" },
-      { 12, "up" },
-      { 13, "down",    "dpad_down" },
-      { 14, "left",    "dpad_left" },
-      { 15, "right",   "dpad_right" },
+      { 12, "up", },
+      { 13, "down", },
+      { 14, "left", },
+      { 15, "right", },
       { 16, "touchpad" },
     },
   }
 }
 
 Joystick.ControlMaps = {
-  -- ["PS4 Controller"] = {
-  --   name = "PS4 Controller",
-  --   numAxes = 5,
-  --   numButtons = 12,
-  --   axisControls = { leftx = 1, lefty = 2, rightx = 3, righty = 4, l2 = 5, r2 = 6 },
-  --   axisNames = {
-  --     [1] = "leftx",
-  --     [2] = "lefty",
-  --     [3] = "rightx",
-  --     [4] = "righty",
-  --     [5] = "l2",
-  --     [6] = "r2",
-  --   },
-  --   buttonControls = {
-  --     face1 = 1, -- cross
-  --     face2 = 2, -- circle
-  --     face3 = 3, -- square
-  --     face4 = 4, -- triangle
-  --     select = 5,
-  --     ps = 6,
-  --     start = 7,
-  --     l3 = 8,
-  --     r3 = 9,
-  --     l1 = 10,
-  --     r1 = 11,
-  --     up = 12,
-  --     down = 13,
-  --     left = 14,
-  --     right = 15,
-  --     touchpad = 16,
-  --   },
-  --   buttonNames = {
-  --     [1] = "face1",
-  --     [2] = "face2",
-  --     [3] = "face3",
-  --     [4] = "face4",
-  --     [5] = "l2",
-  --     [6] = "r2",
-  --     [7] = "l1",
-  --     [8] = "r1",
-  --     [9] = "select",
-  --     [10] = "start",
-  --     [11] = "l3",
-  --     [12] = "r3",
-  --   },
-  -- },
   Dualshock = {
     name = "Dualshock",
     numAxes = 5,
@@ -160,20 +121,28 @@ for _, def in ipairs(Defs) do
   mapping.numButtons = #def.buttons
   mapping.buttonControls = {}
   mapping.buttonNames = {}
-  for _, bdef in ipairs(def.buttons) do
-    mapping.buttonNames[bdef[1]] = bdef[2]
-    for i = 2, #bdef do
-      mapping.buttonControls[bdef[i]] = bdef[1]
-    end
+  mapping.transforms = {} -- index of control ids to transform funcs (optional)
+  for _, buttonDef in ipairs(def.buttons) do
+    -- map button id to button name
+    mapping.buttonNames[buttonDef[1]] = buttonDef[2]
+    -- map button name to button id
+    mapping.buttonControls[buttonDef[2]] = buttonDef[1]
   end
 
   mapping.numAxes = #def.axes
   mapping.axisControls = {}
   mapping.axisNames = {}
-  for _, adef in ipairs(def.axes) do
-    mapping.axisNames[adef[1]] = adef[2]
-    for i = 2, #adef do
-      mapping.axisControls[adef[i]] = adef[1]
+  mapping.axisTransforms = {}
+  for _, axisDef in ipairs(def.axes) do
+    -- map axis id to axis name
+    mapping.axisNames[axisDef[1]] = axisDef[2]
+    -- map axis name to axis id
+    mapping.axisControls[axisDef[2]] = axisDef[1]
+    if axisDef[3] then
+      -- opts: transform
+      if axisDef[3].transform then
+        mapping.axisTransforms[axisDef[1]] = axisDef[3].transform
+      end
     end
   end
 
@@ -194,6 +163,32 @@ function Joystick.getControlMap(name)
   local map = Joystick.ControlMaps[name] or Joystick.ControlMaps.Default
   assert(map, "Joystick: Couldn't find ControlMap for '" .. name .. "'")
   return map
+end
+
+function Joystick.getControlMapForJoystick(joystick)
+  return Joystick.getControlMap(joystick:getName())
+end
+
+local AxisDeadzone = 0.18
+
+-- Small axis values are quashed to 0
+local function applyAxisDeadzone(value)
+  return math.abs(value) >= AxisDeadzone and value or 0
+end
+
+-- If the given joystick config contains a special transform func for the given axis,
+-- apply the transform, otherwise just return value.
+local function transformAxisValue(controlMap, axisId, value)
+  local txfunc = controlMap.axisTransforms and controlMap.axisTransforms[axisId]
+  if txfunc then
+    value = txfunc(value)
+  end
+  return value
+end
+
+-- Apply special transforms and deadzone threshold to control value
+function Joystick.groomAxisValue(controlMap, axis, value)
+  return applyAxisDeadzone(transformAxisValue(controlMap, axis, value))
 end
 
 return Joystick
