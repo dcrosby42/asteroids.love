8c5a5c48969fc638c43cbe6962032148da6a860b
David Crosby <dcrosby42@gmail.com>
2024-06-25 00:25:55 -0400

    castle: more inline docs for Query

--- vendor/castle/ecs/query.lua
+++ vendor/castle/ecs/query.lua
@@ -2,11 +2,10 @@ local Predicates = require "castle.ecs.predicates"
 local Query = {}
 local inspect = require "inspect"
 
+-- indexLookup -> { name=, key= }
+-- filter -> func<e -> bool>
 function Query:new(o)
   o = o or {}
-  -- o.indexLookup = o.indexLookup or nil
-  -- o.filter = o.filter or nil
-
   if o._debug then
     print("Query:new(): " .. inspect(o))
   end
@@ -17,7 +16,8 @@ function Query:new(o)
   return o
 end
 
-function Query:__call(estore)
+-- Accepts an entity store, returns a list of 0 or more matching Entities.
+function Query:execute(estore)
   local ents = {}
   if self.indexLookup then
     ents = estore:indexLookup(self.indexLookup.name, self.indexLookup.key)
@@ -28,14 +28,27 @@ function Query:__call(estore)
   return ents
 end
 
+-- Implement "callable" interface; a Query object may be invoked like a function.
+function Query:__call(estore)
+  return self:execute(estore)
+end
+
+-- Update a queryArgs structure to filter on one or more component types.
+-- Unless an indexLookup already exists in the args, the first comp type becomes an indexLookup.
+-- Remaining comp types are combined using the hasComps predicate composer.
+-- Modifies queryArgs in-place and also returns it.
+-- Does nothing if compTypes is empty.
 local function addCompTypes(queryArgs, compTypes)
-  if #compTypes == 0 then return queryArgs end
+  if not compTypes or #compTypes == 0 then return queryArgs end
   if not queryArgs.indexLookup then
+    -- This queryArgs hasn't got an indexLookup yet.
+    -- Use the first component type as an indexLookup:
     local firstType = compTypes[1]
     queryArgs.indexLookup = { name = "byCompType", key = firstType }
     compTypes = tail(compTypes)
   end
   if #compTypes > 0 then
+    -- Generate a hasComps predicate function based on 1 or more remaining component types
     local matcherFn = Predicates.hasComps(unpack(compTypes))
     if queryArgs.filter then
       queryArgs.filter = Predicates.allOf(queryArgs.filter, matcherFn)
@@ -47,13 +60,13 @@ local function addCompTypes(queryArgs, compTypes)
 end
 
 -- Given a variety of "query args", return proper Query ctor params
--- Sugar:
+-- Sugar: (in order of selection/evaluation)
 --   * string: component-type query
 --   * function: filter-type query using the given predicate
---   * array: multi-comp=type; first type becomes an indexLookup, subsquent types are converted to a hasComps filter
+--   * array: multi-comp-type; first type becomes an indexLookup, subsquent types are converted to a hasComps filter
 --   * obj.tag: use byTag index lookup
---   * obj.tags: add comp-type filters; use byTag index lookup if indexLookup not already specified
---   * obj.name: use byName index lookup
+--   * obj.comp: add comp-type filters use byCompType index lookup, if indexLookup not already specified
+--   * obj.comps: add comp-type filters; use byCompType index lookup, if indexLookup not already specified
 local function expand(args)
   local argsType = type(args)
   if argsType == "string" then
@@ -75,7 +88,6 @@ local function expand(args)
       args.indexLookup = { name = "byTag", key = args.tag }
       args.tag = nil
     end
-    -- TODO: tags...?
     if args.comp then
       addCompTypes(args, { args.comp })
       args.comp = nil
